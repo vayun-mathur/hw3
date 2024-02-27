3e99b82e7f31bb1256448b88dee7586e1c8bcd25
Vayun Mathur <vayunmat@usc.edu>
2024-02-27 21:42:45 +0000

    fixed 2

--- llrec.cpp
+++ llrec.cpp
@@ -5,29 +5,15 @@
 //*********************************************
 
 void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot) {
-    Node *smaller_end = NULL, *larger_end = NULL;
-    smaller = NULL;
-    larger = NULL;
-    while(head) {
-        if(head->val <= pivot) {
-            if(smaller == NULL) {
-                smaller = head;
-                smaller_end = head;
-            } else {
-                smaller_end->next = head;
-                smaller_end = head;
-            }
-        } else {
-            if(larger == NULL) {
-                larger = head;
-                larger_end = head;
-            } else {
-                larger_end->next = head;
-                larger_end = head;
-            }
-        }
-        head = head->next;
+    if(head == NULL) {
+      smaller = NULL;
+      larger = NULL;
     }
-    if(smaller_end) smaller_end->next = NULL;
-    if(larger_end) larger_end->next = NULL;
+    if(head->val <= pivot) {
+      smaller = head;
+      llpivot(head->next, smaller->next, larger, pivot);
+    } else {
+      larger = head;
+      llpivot(head->next, smaller, larger->next, pivot);
+    }
 }
